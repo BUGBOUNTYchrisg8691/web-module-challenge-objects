af8945fea1b222709191af7b8c34f9e4c57e5dc1
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-09-10 00:43:58 -0400

    stretch 2 done

--- index.js
+++ index.js
@@ -149,10 +149,16 @@ and should return an array of objects.
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }]
 */
-function getLongReviews( /* code here */ ) {
-    /* code here */
+function getLongReviews(arr) {
+    const newArr = [];
+    arr.forEach(a => {
+        if (a.feedback.split(" ").length > 15) {
+            newArr.push(a);
+        }
+    });
+    return newArr;
 }
-
+console.log(getLongReviews(reviews));
 
 /* STRETCH 3:  This challenge is not related to the data above! 
 
