e1e04796ffcd72c2ca672b5a4219db9d260d11d4
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-09-10 11:40:02 -0400

    refactor task 1

--- refactor.js
+++ refactor.js
@@ -7,12 +7,16 @@ const breakfastBurrito = { name: "Breakfast Burrito", price: 16, category: "Brea
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
 function createMenuItem(name, cost, category) {
-    const obj = {};
-    obj.name = name;
-    obj.price = cost;
-    obj.category = category;
+    const obj = {
+        name: name,
+        price: cost,
+        category: category
+    };
+    
     return obj;
-}
+};
+
+console.log(createMenuItem("Chicken Nuggets", 7, "Lunch"))
 
 /* Task 1b: use your function to create 3 more menu items. You may add any items to the menu that you'd like */
 
@@ -20,6 +24,9 @@ const waffles = createMenuItem("Belgian Waffles", 14, "Breakfast");
 const shake = createMenuItem("Chocolate Milkshake", 8, "Drinks");
 const chicSand = createMenuItem("Chicken Sandwich", 12, "Lunch");
 
+console.log(waffles);
+console.log(shake);
+console.log(chicSand);
 
 /* Task 2: You're having a lunch special! 25% off for teachers and students, 10% off for everyone else. Add a method to your burger object that automatically calculates price given a string as a parameter. 
 
